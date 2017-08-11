26dee7d8df3a1d3fdef53eafe14f61314e90c8bb
jhon14hd <30933365+jhon14hd@users.noreply.github.com>
2017-08-11 13:59:35 -0400

    Proyecto final
    jhon jairo heredia 14-eisn-1-198

---
+++ Calcular_notas/Appcalcular_nota/Appcalcular_nota/Form1.Designer.vb
@@ -0,0 +1,231 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.nota1 = New System.Windows.Forms.TextBox()
+        Me.nota2 = New System.Windows.Forms.TextBox()
+        Me.nota3 = New System.Windows.Forms.TextBox()
+        Me.nota4 = New System.Windows.Forms.TextBox()
+        Me.nota5 = New System.Windows.Forms.TextBox()
+        Me.resultado = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(106, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(126, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Calcular Notas"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(28, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Primer Parcial"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(11, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(123, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Segundo Parcial"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(50, 155)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 16)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Exposicion"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(33, 209)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(101, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Examen Final"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(50, 255)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(84, 16)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Asistencia "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(55, 296)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 16)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Nota Final"
+        '
+        'nota1
+        '
+        Me.nota1.Location = New System.Drawing.Point(155, 60)
+        Me.nota1.Name = "nota1"
+        Me.nota1.Size = New System.Drawing.Size(143, 20)
+        Me.nota1.TabIndex = 7
+        '
+        'nota2
+        '
+        Me.nota2.Location = New System.Drawing.Point(155, 107)
+        Me.nota2.Name = "nota2"
+        Me.nota2.Size = New System.Drawing.Size(143, 20)
+        Me.nota2.TabIndex = 8
+        '
+        'nota3
+        '
+        Me.nota3.Location = New System.Drawing.Point(155, 155)
+        Me.nota3.Name = "nota3"
+        Me.nota3.Size = New System.Drawing.Size(143, 20)
+        Me.nota3.TabIndex = 9
+        '
+        'nota4
+        '
+        Me.nota4.Location = New System.Drawing.Point(155, 208)
+        Me.nota4.Name = "nota4"
+        Me.nota4.Size = New System.Drawing.Size(143, 20)
+        Me.nota4.TabIndex = 10
+        '
+        'nota5
+        '
+        Me.nota5.Location = New System.Drawing.Point(155, 255)
+        Me.nota5.Name = "nota5"
+        Me.nota5.Size = New System.Drawing.Size(143, 20)
+        Me.nota5.TabIndex = 11
+        '
+        'resultado
+        '
+        Me.resultado.Location = New System.Drawing.Point(155, 296)
+        Me.resultado.Name = "resultado"
+        Me.resultado.Size = New System.Drawing.Size(143, 20)
+        Me.resultado.TabIndex = 12
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(36, 341)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 23)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(192, 341)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(106, 23)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Limpiar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(72, 377)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(188, 25)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "Datos Del Estudiante "
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(367, 414)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.resultado)
+        Me.Controls.Add(Me.nota5)
+        Me.Controls.Add(Me.nota4)
+        Me.Controls.Add(Me.nota3)
+        Me.Controls.Add(Me.nota2)
+        Me.Controls.Add(Me.nota1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calcular Notas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents nota1 As System.Windows.Forms.TextBox
+    Friend WithEvents nota2 As System.Windows.Forms.TextBox
+    Friend WithEvents nota3 As System.Windows.Forms.TextBox
+    Friend WithEvents nota4 As System.Windows.Forms.TextBox
+    Friend WithEvents nota5 As System.Windows.Forms.TextBox
+    Friend WithEvents resultado As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ Calcular_notas/Appcalcular_nota/Appcalcular_nota/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        resultado.Text = Val(nota1.Text) + Val(nota2.Text) + Val(nota3.Text) + Val(nota4.Text) + Val(nota5.Text)
+   
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        nota1.Clear()
+        nota2.Clear()
+        nota3.Clear()
+        nota4.Clear()
+        nota5.Clear()
+        resultado.Clear()
+        nota1.Focus()
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("Nombre: Jhon Jairo Heredia, Matricula: 14-EISN-1-198 ", "Datos Del Estudiante")
+
+    End Sub
+End Class
